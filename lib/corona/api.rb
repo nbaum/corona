006002638ffee7aa1789e88656ace76d3609baed
Nathan Baum <n@p12a.org.uk>
2014-03-25 02:59:05 +0000

    Change dispatch style

--- lib/corona/api.rb
+++ lib/corona/api.rb
@@ -3,43 +3,51 @@ module Corona
   
   class API
     
+    attr_reader :params
+    
     def call (env)
       @request = Rack::Request.new(env)
       @response = Rack::Response.new
       dispatch
     end
     
-    def dispatch
-      args = YAML.load(@request.body.read)
+    def do_log ()
+      instance.log
+    end
+    
+    def do_stop ()
+      i = instance
+      i.stop
+      sleep 0.1 while i.running?
+      true
+    end
+    
+    def do_start ()
+      instance.config = params[:config]
+      instance.start
+      true
+    end
+    
+    def do_status ()
+      instance.status
+    end
+    
+    private
+    
+    def instance
+      Instance[params[:instance]]
+    end
+    
+    def dispatch ()
+      @params = YAML.load(@request.body.read)
       name = @request.path[1..-1]
-      res = __send__("do_" + name, args)
+      res = __send__("do_" + name)
       @response.write res.to_yaml
       @response.finish
     rescue => e
       @response.write [e.message, e.backtrace].to_yaml
       @response.status = 500
       @response.finish
-    end
-    
-    def do_log (instance: nil)
-      Instance[instance].log
-    end
-    
-    def do_stop (instance: nil)
-      i = Instance[instance]
-      i.stop
-      sleep 0.1 while i.running?
-      true
-    end
-    
-    def do_start (instance: nil, config: nil)
-      Instance[instance].config = config
-      Instance[instance].start
-      true
-    end
-    
-    def do_status (instance: nil)
-      Instance[instance].status
     end
     
   end
