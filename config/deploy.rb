475414657a5609f3774d7feb35af150a8c5b1f91
Nathan Baum <n@p12a.org.uk>
2014-04-28 11:43:27 +0100

    Trim deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config valid only for Capistrano 3.2
+lock '3.2.0'
 
 set :application, 'corona'
 set :repo_url, 'corona@84.45.122.187:repo'
@@ -9,26 +9,3 @@ set :deploy_to, '/home/corona'
 set :linked_dirs, %w{var}
 
 set :keep_releases, 5
-
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
