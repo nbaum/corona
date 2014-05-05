38a7640d51616349402fc283329e39b63292ff08
Nathan Baum <n@p12a.org.uk>
2014-05-05 18:08:19 +0100

    Update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.2
-lock '3.2.0'
+lock '3.2.1'
 
 set :application, 'corona'
 set :repo_url, 'corona@84.45.122.187:repo'
@@ -9,3 +9,13 @@ set :deploy_to, '/home/corona'
 set :linked_dirs, %w{var}
 
 set :keep_releases, 5
+
+namespace :deploy do
+  task :restart do
+    on roles(:app), in: :sequence, wait: 2 do
+      execute "tmux kill-session -t corona" rescue
+      execute "tmux new-session -d -s corona /home/corona/run"
+    end
+  end
+end
+
