ed2082c6d37ab877efce4d1d193b7f386a5bbfac
Nathan Baum <n@p12a.org.uk>
2015-04-24 02:04:24 +0100

    New compute nodes

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,13 @@
-role :app, %w{corona@10.2.12.4
-              corona@46.30.8.50}
+role :app, %w(
+	corona@10.2.12.4
+	corona@46.30.8.50
+	corona@10.2.12.100
+	corona@10.2.12.101
+)
 
-role :sys, %w{root@10.2.12.4
-              root@46.30.8.50}
+role :sys, %w(
+	root@10.2.12.4
+	root@46.30.8.50
+	root@10.2.12.100
+	root@10.2.12.101
+)
